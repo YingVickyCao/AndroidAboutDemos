4bb0e273cc403b399518ca4655e16a905ee3cde1
YingVickyCao <ying.vicky.cao@outlook.com>
2018-05-28 23:08:17 +0800

    StringQA

---
+++ soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/string_stringbuffer_stringbuilder/qa/StringQA.java
@@ -0,0 +1,11 @@
+package com.hades.android.example.android_about_demos.string_stringbuffer_stringbuilder.qa;
+
+public class StringQA {
+    /**
+     * 创建了几个String Object？
+     *
+     */
+    public void qa() {
+        String s = new String("xyz");
+    }
+}
