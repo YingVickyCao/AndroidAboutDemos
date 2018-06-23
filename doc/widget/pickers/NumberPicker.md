689dc00a8947961c86549ffec5ddfbaa6b58f443
YingVickyCao <ying.vicky.cao@outlook.com>
2018-06-23 22:15:44 +0800

    NumberPicker

--- doc/widget/pickers/NumberPicker.md
+++ doc/widget/pickers/NumberPicker.md
@@ -10,12 +10,23 @@
 ```
 np.setMinValue(1);
 np.setMaxValue(10);
-np.setValue(5); // set current value
-    
+np.setValue(5); // init value
+NumberPicker.getValue() // 获取当前选定值，返回值int类型
+NumberPicker.setFormatter(Formatter formatter) 
+
 ```
 ---
 
 ## EVENT
+
 ```
 np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+```
+
+```
+NumberPicker.setOnScrollListener(OnScrollListener onScrollListener) 
+滑动状态变化监听事件，分为3种状态：
+SCROLL_STATE_TOUCH_SCROLL：用户按下去然后滑动
+SCROLL_STATE_FLING： 正在滑动中的状态
+SCROLL_STATE_IDLE： NumberPicker停止滑动
 ```
--- soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/widget/pickers/NumberPickerFragment.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/widget/pickers/NumberPickerFragment.java
@@ -1,6 +1,7 @@
 package com.hades.android.example.android_about_demos.widget.pickers;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -8,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.hades.android.example.android_about_demos.R;
 
@@ -30,15 +32,67 @@ public class NumberPickerFragment extends Fragment {
         NumberPicker np = view.findViewById(R.id.np);
         TextView mShow = view.findViewById(R.id.show);
 
-        np.setMinValue(1);
-        np.setMaxValue(10);
-        np.setValue(5);
+        /**
+         * 格式化NumberPicker内部的数字
+         */
+        np.setFormatter(new NumberPicker.Formatter() {
+            @Override
+            public String format(int value) {
+                // TODO: 23/06/2018 NumberPicker.setFormatter
+//                return String.valueOf(value);
+                return "Num = " + String.valueOf(value);
+            }
+        });
+
+        /**
+         * 值变化监听事件
+         */
         np.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
             @Override
             public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-                mShow.setText(String.valueOf(newVal));
+//                mShow.setText(String.valueOf(newVal));
+                mShow.setText(String.valueOf(picker.getValue()));
             }
         });
+
+        /**
+         * 滑动状态变化监听事
+         */
+        np.setOnScrollListener(new NumberPicker.OnScrollListener() {
+            @Override
+            public void onScrollStateChange(NumberPicker view, int scrollState) {
+                switch (scrollState) {
+                    /**
+                     * 用户按下去然后滑动
+                     */
+                    case SCROLL_STATE_TOUCH_SCROLL:
+                        Toast.makeText(getUsedContext(), "scroll state touch scroll", Toast.LENGTH_LONG).show();
+                        break;
+
+                    /**
+                     * 正在滑动中的状态
+                     */
+                    case SCROLL_STATE_FLING:
+                        Toast.makeText(getUsedContext(), "scroll state fling", Toast.LENGTH_LONG).show();
+                        break;
+
+                    /**
+                     * 停止滑动
+                     */
+                    case SCROLL_STATE_IDLE:
+                        Toast.makeText(getUsedContext(), "scroll state idle", Toast.LENGTH_LONG).show();
+                        break;
+                }
+            }
+        });
+
+        np.setMinValue(1);
+        np.setMaxValue(10);
+        np.setValue(5);
         return view;
+    }
+
+    private Context getUsedContext() {
+        return getActivity();
     }
 }
