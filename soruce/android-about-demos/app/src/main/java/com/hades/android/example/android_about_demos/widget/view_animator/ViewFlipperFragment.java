cf02678c69b052cfd6e61d215c74b44f5934972e
YingVickyCao <ying.vicky.cao@outlook.com>
2018-06-23 14:54:58 +0800

    禁止轮询

--- soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/widget/view_animator/ViewFlipperFragment.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/widget/view_animator/ViewFlipperFragment.java
@@ -4,14 +4,25 @@ import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextSwitcher;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import com.hades.android.example.android_about_demos.R;
 
 public class ViewFlipperFragment extends Fragment {
+    private static final String TAG = ViewFlipperFragment.class.getSimpleName();
+
+    private String[] mShowedTextArray = {"1", "2", "3"};
+    private int mShowedTextIndex;
+
     private ViewFlipper viewFlipper;
 
     public static ViewFlipperFragment newInstance() {
@@ -31,6 +42,18 @@ public class ViewFlipperFragment extends Fragment {
         view.findViewById(R.id.auto).setOnClickListener(this::auto);
         view.findViewById(R.id.stopAuto).setOnClickListener(this::stopAuto);
 
+        for (int i = 0; i < 3; i++) {
+            mShowedTextIndex = i;
+            TextView textView = new TextView(getUsedContext());
+            textView.setTextSize(100);
+            textView.setLayoutParams(new TextSwitcher.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+            textView.setGravity(Gravity.CENTER);
+            textView.setText(mShowedTextArray[mShowedTextIndex]);
+            viewFlipper.addView(textView);
+        }
+
+        Log.d(TAG, "onCreateView: getChildCount=" + viewFlipper.getChildCount());
+        Log.d(TAG, "onCreateView: getDisplayedChild=" + viewFlipper.getDisplayedChild());
         return view;
     }
 
@@ -39,6 +62,15 @@ public class ViewFlipperFragment extends Fragment {
     }
 
     public void prev(View source) {
+        /**
+         * 点击切换 View时，禁止轮询。
+         */
+        if (isFirst()) {
+            Toast.makeText(getUsedContext(), "Already first one", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Log.d(TAG, "prev: getChildCount=" + viewFlipper.getChildCount());
+        Log.d(TAG, "prev: getDisplayedChild=" + viewFlipper.getDisplayedChild());
         viewFlipper.setInAnimation(getUsedContext(), R.anim.slide_in_right);
         viewFlipper.setOutAnimation(getUsedContext(), R.anim.slide_out_left);
         viewFlipper.showPrevious();
@@ -46,10 +78,28 @@ public class ViewFlipperFragment extends Fragment {
     }
 
     public void next(View source) {
+        Log.d(TAG, "next: getChildCount=" + viewFlipper.getChildCount());
+        Log.d(TAG, "next: getDisplayedChild=" + viewFlipper.getDisplayedChild());
+
+        /**
+         * 点击切换 View时，禁止轮询。
+         */
+        if (isLast()) {
+            Toast.makeText(getUsedContext(), "Already last one", Toast.LENGTH_SHORT).show();
+            return;
+        }
         viewFlipper.setInAnimation(getUsedContext(), android.R.anim.slide_in_left);
         viewFlipper.setOutAnimation(getUsedContext(), android.R.anim.slide_out_right);
         viewFlipper.showNext();
         viewFlipper.stopFlipping();
+    }
+
+    private boolean isLast() {
+        return viewFlipper.getDisplayedChild() == (viewFlipper.getChildCount() - 1);
+    }
+
+    private boolean isFirst() {
+        return viewFlipper.getDisplayedChild() == 0;
     }
 
     public void auto(View source) {
@@ -61,5 +111,4 @@ public class ViewFlipperFragment extends Fragment {
     public void stopAuto(View source) {
         viewFlipper.stopFlipping();
     }
-
 }
