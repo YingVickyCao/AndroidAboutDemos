f7cde1c41bf5b1770b8c8d6b569abfe261a1c518
YingVickyCao <ying.vicky.cao@outlook.com>
2018-07-02 06:14:52 +0800

    数组

--- soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/resource/ResourceActivity.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/resource/ResourceActivity.java
@@ -5,24 +5,33 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.widget.Button;
 
 import com.hades.android.example.android_about_demos.R;
 
 public class ResourceActivity extends Activity {
     private static final String TAG = ResourceActivity.class.getSimpleName();
 
+    Button normalArrayBtn;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resources);
 
-        findViewById(R.id.getNormalArray).setOnClickListener(v -> getNormalArray());
+        normalArrayBtn = findViewById(R.id.getNormalArray);
+        normalArrayBtn.setOnClickListener(v -> getNormalArray());
         findViewById(R.id.getStringArray).setOnClickListener(v -> getStringArray());
         findViewById(R.id.getIntArray).setOnClickListener(v -> getIntArray());
     }
 
     private void getNormalArray() {
         TypedArray typedArray = getResources().obtainTypedArray(R.array.normal_array);
+//        Drawable drawable = typedArray.getDrawable(0);
+//        normalArrayBtn.setBackground(drawable);
+
+        int  color = typedArray.getColor(1,0);
+        normalArrayBtn.setBackgroundColor(color);
     }
 
     private void getStringArray() {
@@ -31,7 +40,7 @@ public class ResourceActivity extends Activity {
          */
         String[] strings = getResources().getStringArray(R.array.string_array);
         String str = "";
-        for (int i = 0; i< strings.length; i++){
+        for (int i = 0; i < strings.length; i++) {
             str += strings[i] + ",";
         }
         Log.d(TAG, "getStringArray: " + str);
@@ -43,7 +52,7 @@ public class ResourceActivity extends Activity {
          */
         int[] ints = getResources().getIntArray(R.array.int_array);
         String str = "";
-        for (int i = 0; i< ints.length; i++){
+        for (int i = 0; i < ints.length; i++) {
             str += ints[i] + ",";
         }
         Log.d(TAG, "getIntArray: " + str);
