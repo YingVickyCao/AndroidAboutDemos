8615038afe2af0692d79053d11504fff20e50e1e
YingVickyCao <ying.vicky.cao@outlook.com>
2018-07-03 07:50:47 +0800

    animation

--- soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/resource/anim/AnimationDrawableActivity.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/resource/anim/AnimationDrawableActivity.java
@@ -2,10 +2,8 @@ package com.hades.android.example.android_about_demos.resource.anim;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Button;
 import android.widget.ImageView;
 
 import com.hades.android.example.android_about_demos.R;
@@ -17,30 +15,36 @@ public class AnimationDrawableActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        image = (ImageView) findViewById(R.id.image);
+        image = findViewById(R.id.image);
 
-        Button bn = (Button) findViewById(R.id.bn);
-        bn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                // 加载动画资源
-                final Animation anim = AnimationUtils.loadAnimation(AnimationDrawableActivity.this, R.anim.my_anim);
-                // 设置动画结束后保留结束状态
-                anim.setFillAfter(true);  // ①
-
-                // 开始动画
-                image.startAnimation(anim);
-            }
-        });
-
+        findViewById(R.id.alpha).setOnClickListener(v -> alpha());
         findViewById(R.id.scale).setOnClickListener(v -> scale());
         findViewById(R.id.translate).setOnClickListener(v -> translate());
         findViewById(R.id.rotate).setOnClickListener(v -> rotate());
+        findViewById(R.id.combineAnim).setOnClickListener(v -> combineAnim());
+    }
+
+    /**
+     * https://developer.android.google.cn/guide/topics/resources/animation-resource
+     *
+     * https://www.cnblogs.com/dongweiq/p/6863521.html
+     * https://www.cnblogs.com/carbs/p/5302908.html
+     * <p>
+     * LayerAnimation
+     * https://blog.csdn.net/witernie/article/details/55050965
+     * <p>
+     * AlphaAnimation
+     * https://www.cnblogs.com/sishuiliuyun/p/3167581.html
+     */
+    private void alpha() {
+        final Animation anim = AnimationUtils.loadAnimation(AnimationDrawableActivity.this, R.anim.anim_alpha);
+        // 设置动画结束后保留结束状态
+        anim.setFillAfter(false);
+        image.startAnimation(anim);
     }
 
     private void scale() {
         final Animation anim = AnimationUtils.loadAnimation(AnimationDrawableActivity.this, R.anim.anim_scale);
-        // 设置动画结束后保留结束状态
         anim.setFillAfter(false);
         image.startAnimation(anim);
     }
@@ -52,6 +56,12 @@ public class AnimationDrawableActivity extends Activity {
     }
 
     private void rotate() {
+        final Animation anim = AnimationUtils.loadAnimation(AnimationDrawableActivity.this, R.anim.anim_rotate);
+        anim.setFillAfter(false);
+        image.startAnimation(anim);
+    }
+
+    private void combineAnim() {
         final Animation anim = AnimationUtils.loadAnimation(AnimationDrawableActivity.this, R.anim.anim_rotate);
         anim.setFillAfter(false);
         image.startAnimation(anim);
