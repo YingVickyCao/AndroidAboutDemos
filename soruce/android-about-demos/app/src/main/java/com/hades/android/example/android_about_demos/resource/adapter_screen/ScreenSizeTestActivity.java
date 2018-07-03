e513e5ea0e70f6a45465fe30cc54f92ff8e7c92c
YingVickyCao <ying.vicky.cao@outlook.com>
2018-07-03 22:42:53 +0800

    屏幕自适应

---
+++ soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/resource/adapter_screen/ScreenSizeTestActivity.java
@@ -0,0 +1,18 @@
+package com.hades.android.example.android_about_demos.resource.adapter_screen;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hades.android.example.android_about_demos.R;
+
+
+public class ScreenSizeTestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.res_screen_size);
+    }
+}
+
