4859bdd2a2c99a073267e31ee7c421b5bd0a7650
YingVickyCao <ying.vicky.cao@outlook.com>
2018-07-17 23:01:14 +0800

    TELEPHONY_SERVICE

--- soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/app_component/service/system/TelephonyServiceActivity.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/app_component/service/system/TelephonyServiceActivity.java
@@ -34,10 +34,10 @@ public class TelephonyServiceActivity extends Activity {
     private boolean mIsHasPermission = false;
 
     ListView showView;
-    // 声明代表状态名的数组
     String[] statusNames;
-    // 声明代表手机状态的集合
-    ArrayList<String> statusValues = new ArrayList<>();
+    private ArrayList<String> statusValues = new ArrayList<>();
+    private ArrayList<Map<String, String>> status = new ArrayList<>();
+    private SimpleAdapter adapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -47,24 +47,37 @@ public class TelephonyServiceActivity extends Activity {
         rxPermissions = new RxPermissions(this);
         rxPermissions.setLogging(true);
 
-        findViewById(R.id.checkPermission).setOnClickListener(v -> checkPermission());
+        showView = findViewById(R.id.show);
 
-        // 获取系统的TelephonyManager对象
-        TelephonyManager tManager = (TelephonyManager)
-                getSystemService(Context.TELEPHONY_SERVICE);
+        adapter = buildSimpleAdapter(status);
+        showView.setAdapter(adapter);
+
+        findViewById(R.id.checkPermission).setOnClickListener(v -> checkPermission());
+        findViewById(R.id.getData).setOnClickListener(v -> getData());
+    }
+
+    private void getData() {
+        status.clear();
+        statusValues.clear();
+
+        buildData();
+        status = buildListData();
+        adapter.notifyDataSetChanged();
+    }
+
+    private void buildData() {
+        // 获取系统的 TelephonyManager 对象
+        TelephonyManager tManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
         // 获取各种状态名称的数组
         statusNames = getResources().getStringArray(R.array.statusNames);
         // 获取代表SIM卡状态的数组
-        String[] simState = getResources()
-                .getStringArray(R.array.simState);
+        String[] simState = getResources().getStringArray(R.array.simState);
         // 获取代表电话网络类型的数组
-        String[] phoneType = getResources().getStringArray(
-                R.array.phoneType);
+        String[] phoneType = getResources().getStringArray(R.array.phoneType);
         // 获取设备编号
         statusValues.add(tManager.getDeviceId());
         // 获取系统平台的版本
-        statusValues.add(tManager.getDeviceSoftwareVersion()
-                != null ? tManager.getDeviceSoftwareVersion() : "未知");
+        statusValues.add(tManager.getDeviceSoftwareVersion() != null ? tManager.getDeviceSoftwareVersion() : "未知");
         // 获取网络运营商代号
         statusValues.add(tManager.getNetworkOperator());
         // 获取网络运营商名称
@@ -72,35 +85,31 @@ public class TelephonyServiceActivity extends Activity {
         // 获取手机网络类型
         statusValues.add(phoneType[tManager.getPhoneType()]);
         // 获取设备所在位置
-        statusValues.add(tManager.getCellLocation() != null ? tManager
-                .getCellLocation().toString() : "未知位置");
+        statusValues.add(tManager.getCellLocation() != null ? tManager.getCellLocation().toString() : "未知位置");
         // 获取SIM卡的国别
         statusValues.add(tManager.getSimCountryIso());
         // 获取SIM卡序列号
         statusValues.add(tManager.getSimSerialNumber());
         // 获取SIM卡状态
         statusValues.add(simState[tManager.getSimState()]);
-        // 获得ListView对象
-        showView = (ListView) findViewById(R.id.show);
-        ArrayList<Map<String, String>> status = new ArrayList<>();
-        // 遍历statusValues集合，将statusNames、statusValues
-        // 的数据封装到List<Map<String , String>>集合中
+    }
+
+    private ArrayList<Map<String, String>> buildListData() {
         for (int i = 0; i < statusValues.size(); i++) {
             HashMap<String, String> map = new HashMap<>();
             map.put("name", statusNames[i]);
             map.put("value", statusValues.get(i));
             status.add(map);
         }
-        // 使用SimpleAdapter封装List数据
-        SimpleAdapter adapter = new SimpleAdapter(this, status,
-                R.layout.line, new String[]{"name", "value"}
-                , new int[]{R.id.name, R.id.value});
-        // 为ListView设置Adapter
-        showView.setAdapter(adapter);
+        return status;
+    }
+
+    private SimpleAdapter buildSimpleAdapter(ArrayList<Map<String, String>> status) {
+        return new SimpleAdapter(this, status, R.layout.line, new String[]{"name", "value"}, new int[]{R.id.name, R.id.value});
     }
 
     private void checkPermission() {
-        if (!rxPermissions.isGranted(Manifest.permission.WRITE_CONTACTS) || !rxPermissions.isGranted(Manifest.permission.READ_CONTACTS)) {
+        if (!rxPermissions.isGranted(Manifest.permission.READ_PHONE_STATE) || !rxPermissions.isGranted(Manifest.permission.ACCESS_COARSE_LOCATION)) {
             askUser2GrantPermissions();
             return;
         } else {
@@ -109,7 +118,7 @@ public class TelephonyServiceActivity extends Activity {
     }
 
     private void requestPermission() {
-        rxPermissions.request(Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS).subscribe(new Observer<Boolean>() {
+        rxPermissions.request(Manifest.permission.READ_PHONE_STATE, Manifest.permission.ACCESS_COARSE_LOCATION).subscribe(new Observer<Boolean>() {
             @Override
             public void onSubscribe(Disposable d) {
             }
@@ -137,7 +146,7 @@ public class TelephonyServiceActivity extends Activity {
     }
 
     private void askUser2GrantPermissions() {
-        rxPermissions.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_CONTACTS, Manifest.permission.READ_CONTACTS).subscribe(new Consumer<Boolean>() {
+        rxPermissions.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_PHONE_STATE, Manifest.permission.ACCESS_COARSE_LOCATION).subscribe(new Consumer<Boolean>() {
             @Override
             public void accept(Boolean shouldShowRequestPermissionRationale) throws Exception {
                 if (shouldShowRequestPermissionRationale) {
