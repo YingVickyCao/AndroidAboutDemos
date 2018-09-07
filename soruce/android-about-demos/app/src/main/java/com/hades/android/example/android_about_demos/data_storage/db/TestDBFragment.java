96e0194fcf27051901d82504f66a1dbe7c70c263
Vicky <ying.vicky.cao@outlook.com>
2018-09-07 09:12:35 +0800

    reafactor

--- soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/data_storage/db/TestDBFragment.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/data_storage/db/TestDBFragment.java
@@ -37,8 +37,8 @@ public class TestDBFragment extends BaseFragment {
         mTableContentListView = view.findViewById(R.id.tableContentList);
         mTitleView = view.findViewById(R.id.title);
         mContentView = view.findViewById(R.id.content);
-        view.findViewById(R.id.insertClick).setOnClickListener(v -> insertClick());
-
+        view.findViewById(R.id.insertDictClick).setOnClickListener(v -> insertBundlesData());
+        view.findViewById(R.id.insertBundlesData).setOnClickListener(v -> insertClick());
         return view;
     }
 
@@ -56,21 +56,21 @@ public class TestDBFragment extends BaseFragment {
         }
         try {
             insertData(db, title, content);
-            Cursor cursor = db.rawQuery("select * from news_inf", null);
+            Cursor cursor = db.rawQuery("select * from news_info", null);
             inflateList(cursor);
         } catch (SQLiteException se) {
-            // 执行DDL创建数据表
-            db.execSQL("create table news_inf(_id integer" + " primary key autoincrement," + " news_title varchar(50)," + " news_content varchar(255))");
-            // 执行insert语句插入数据
+            db.execSQL("create table news_info(id integer" + " primary key autoincrement," + " news_title varchar(50)," + " news_content varchar(255))");
             insertData(db, title, content);
-            // 执行查询
-            Cursor cursor = db.rawQuery("select * from news_inf", null);
+            Cursor cursor = db.rawQuery("select * from news_info", null);
             inflateList(cursor);
         }
     }
 
+    private void insertBundlesData() {
+    }
+
     private void insertData(SQLiteDatabase db, String title, String content) {
-        db.execSQL("insert into news_inf values(null , ? , ?)", new String[]{title, content});
+        db.execSQL("insert into news_info values(null , ? , ?)", new String[]{title, content});
     }
 
     private void inflateList(Cursor cursor) {
