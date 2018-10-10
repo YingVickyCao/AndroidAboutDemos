bfa2c223cf9d7265d45d85c2a3d5453e9f6148f6
Vicky <ying.vicky.cao@outlook.com>
2018-10-11 00:13:30 +0800

    LinearLayout：layout_weight,  baselineAligned, layout_grivity, grivity

--- soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/widget/linearlayout/LinearLayoutActivity.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/widget/linearlayout/LinearLayoutActivity.java
@@ -11,24 +11,6 @@ public class LinearLayoutActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linearlayout_home_layout);
-
         initViews();
-
-        findViewById(R.id.linearlayout1).setOnClickListener(v -> linearlayout1());
-        findViewById(R.id.linearlayout2).setOnClickListener(v -> linearlayout2());
-        findViewById(R.id.linearlayout3).setOnClickListener(v -> linearlayout3());
-    }
-
-    private void linearlayout1() {
-        showFragment(new LinearLayout1Fragment());
-    }
-
-
-    private void linearlayout2() {
-        showFragment(new LinearLayout2Fragment());
-    }
-
-    private void linearlayout3() {
-        showFragment(new LinearLayout3Fragment());
     }
 }
