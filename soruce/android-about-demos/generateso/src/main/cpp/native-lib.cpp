44778ef7465c169af0884875329dd09c6305cfb5
Vicky <ying.vicky.cao@outlook.com>
2018-10-18 05:18:40 +0800

    add ndk config

--- soruce/android-about-demos/generateso/src/main/cpp/native-lib.cpp
+++ soruce/android-about-demos/generateso/src/main/cpp/native-lib.cpp
@@ -3,7 +3,7 @@
 
 // MainActivity
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_example_hades_generateso_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */) {
+    Java_com_example_hades_generateso_MainActivity_stringFromJNI(JNIEnv *env, jobject /* this */) {
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
