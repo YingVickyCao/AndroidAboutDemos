13b6bac842dd880b30b5a97c893c60393c308fc5
Vicky <ying.vicky.cao@outlook.com>
2018-10-19 17:24:16 +0800

    Saving and restore state

--- soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/app_component/activity/ActivityTestHomeActivity.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/app_component/activity/ActivityTestHomeActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 
 import com.hades.android.example.android_about_demos.R;
+import com.hades.android.example.android_about_demos.app_component.activity._sate.TestSaveAndRestoreStateActivity;
 import com.hades.android.example.android_about_demos.app_component.activity.lifecycle.A;
 import com.hades.android.example.android_about_demos.base.BaseActivity;
 
@@ -16,12 +17,18 @@ public class ActivityTestHomeActivity extends BaseActivity {
 
         findViewById(R.id.pageActivityLifecycle).setOnClickListener(v -> pageActivityLifecycle());
         findViewById(R.id.pageGetViewSizeInActivityOncreate).setOnClickListener(v -> pageGetViewSizeInActivityOncreate());
+        findViewById(R.id.pageSaveAndRestoreState).setOnClickListener(v -> pageSaveAndRestoreState());
     }
-    private void pageActivityLifecycle(){
+
+    private void pageActivityLifecycle() {
         showActivity(A.class);
     }
 
-    private void pageGetViewSizeInActivityOncreate(){
+    private void pageGetViewSizeInActivityOncreate() {
         showActivity(GetViewSizeInOncreateActivity.class);
+    }
+
+    private void pageSaveAndRestoreState() {
+        showActivity(TestSaveAndRestoreStateActivity.class);
     }
 }
