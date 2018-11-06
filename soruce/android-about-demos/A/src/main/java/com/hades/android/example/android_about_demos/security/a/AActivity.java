0d2f02916e2ecf1fd9f3b06aae87a3f0c40a1284
Vicky <ying.vicky.cao@outlook.com>
2018-11-06 17:52:03 +0800

    Activity can be launched by components from other app

--- soruce/android-about-demos/A/src/main/java/com/hades/android/example/android_about_demos/security/a/AActivity.java
+++ soruce/android-about-demos/A/src/main/java/com/hades/android/example/android_about_demos/security/a/AActivity.java
@@ -12,7 +12,8 @@ public class AActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.security_a);
         findViewById(R.id.jumpB).setOnClickListener(v -> jumpB());
-        findViewById(R.id.jumpC).setOnClickListener(v -> jumpC());
+        findViewById(R.id.jumpC4ImplicitIntent).setOnClickListener(v -> jumpC4ImplicitIntent());
+        findViewById(R.id.jumpC).setOnClickListener(v -> jumpC4ExplicitIntent());
     }
 
     private void jumpB() {
@@ -20,17 +21,33 @@ public class AActivity extends AppCompatActivity {
                 "com.hades.android.example.android_about_demos.security.b.BActivity");
         Intent intent = new Intent();
         intent.setComponent(componentName);
-        intent.setType("text/plain");
         intent.putExtra("NUM", 100);
         startActivity(intent);
     }
 
-    private void jumpC() {
+    private void jumpC4ExplicitIntent() {
+        Intent intent = new Intent();
+        // setComponent
         ComponentName componentName = new ComponentName("com.hades.android.example.android_about_demos.security.b",
                 "com.hades.android.example.android_about_demos.security.b.CActivity");
+//        intent.setComponent(componentName);
+
+        // setClassName
+        intent.setClassName("com.hades.android.example.android_about_demos.security.b",
+                "com.hades.android.example.android_about_demos.security.b.CActivity");
+        intent.putExtra("NUM", 100);
+        startActivity(intent);
+    }
+
+    /*
+    In B App, C :
+    android:exported="true", and no <intent-filter>
+     ERROR: android.content.ActivityNotFoundException: No Activity found to handle Intent { act=com.hades.android.example.android_about_demos.security.b.c cat=[android.intent.category.DEFAULT] (has extras) }
+     */
+    private void jumpC4ImplicitIntent() {
         Intent intent = new Intent();
-        intent.setComponent(componentName);
-        intent.setType("text/plain");
+        intent.setAction("com.hades.android.example.android_about_demos.security.b.c");
+        intent.addCategory(Intent.CATEGORY_DEFAULT);
         intent.putExtra("NUM", 100);
         startActivity(intent);
     }
