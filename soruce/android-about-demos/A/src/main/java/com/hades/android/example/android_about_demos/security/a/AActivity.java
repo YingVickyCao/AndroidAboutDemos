4c208523f703f6b8949286214208cbef412b5ef1
Vicky <ying.vicky.cao@outlook.com>
2018-11-06 17:02:11 +0800

    android:sharedUserId

--- soruce/android-about-demos/A/src/main/java/com/hades/android/example/android_about_demos/security/a/AActivity.java
+++ soruce/android-about-demos/A/src/main/java/com/hades/android/example/android_about_demos/security/a/AActivity.java
@@ -1,5 +1,7 @@
 package com.hades.android.example.android_about_demos.security.a;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,14 +12,26 @@ public class AActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.security_a);
         findViewById(R.id.jumpB).setOnClickListener(v -> jumpB());
+        findViewById(R.id.jumpC).setOnClickListener(v -> jumpC());
     }
 
     private void jumpB() {
-//        Intent intent = new Intent();
-//        intent.setClassName("com.hades.android.example.android_about_demos.security.b"
-//                , "com.hades.android.example.android_about_demos.security.b.BActivity");
-//        intent.setType("text/plain");
-//        intent.putExtra("NUM", 100);
-//        startActivity(intent);
+        ComponentName componentName = new ComponentName("com.hades.android.example.android_about_demos.security.b",
+                "com.hades.android.example.android_about_demos.security.b.BActivity");
+        Intent intent = new Intent();
+        intent.setComponent(componentName);
+        intent.setType("text/plain");
+        intent.putExtra("NUM", 100);
+        startActivity(intent);
+    }
+
+    private void jumpC() {
+        ComponentName componentName = new ComponentName("com.hades.android.example.android_about_demos.security.b",
+                "com.hades.android.example.android_about_demos.security.b.CActivity");
+        Intent intent = new Intent();
+        intent.setComponent(componentName);
+        intent.setType("text/plain");
+        intent.putExtra("NUM", 100);
+        startActivity(intent);
     }
 }
---
+++ soruce/android-about-demos/B/src/main/java/com/hades/android/example/android_about_demos/security/b/CActivity.java
@@ -0,0 +1,13 @@
+package com.hades.android.example.android_about_demos.security.b;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.security_b);
+    }
+}
