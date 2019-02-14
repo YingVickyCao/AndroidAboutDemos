d43fd65025e37e3f6027079641eb068b31e2dcb9
Vicky <ying.vicky.cao@outlook.com>
2019-02-14 22:57:47 +0800

    FrameLayout android:layout_gravity="center"

--- soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/widget/common/QAAboutViewFragemnt.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/android/example/android_about_demos/widget/common/QAAboutViewFragemnt.java
@@ -3,15 +3,18 @@ package com.hades.android.example.android_about_demos.widget.common;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.hades.android.example.android_about_demos.R;
 
 public class QAAboutViewFragemnt extends Fragment {
     ViewGroup redBtnContainer;
     View redBtn;
+    View testedLayoutGravityView;
 
     @Nullable
     @Override
@@ -19,11 +22,12 @@ public class QAAboutViewFragemnt extends Fragment {
         View view = inflater.inflate(R.layout.wiget_qa_about_view, container, false);
 
         redBtnContainer = view.findViewById(R.id.redBtnContainer);
+        testedLayoutGravityView = view.findViewById(R.id.testedLayoutGravityView);
 
         view.findViewById(R.id.checkIfHaveAddedView).setOnClickListener(v -> checkIfHaveAddedView());
+        view.findViewById(R.id.frameLayoutLayoutGravity).setOnClickListener(v -> layoutGravity());
         return view;
     }
-
 
     private void checkIfHaveAddedView() {
         if (null == redBtnContainer.findViewById(R.id.red_btn_root)) {
@@ -38,5 +42,11 @@ public class QAAboutViewFragemnt extends Fragment {
 
     private View getRedBtn() {
         return View.inflate(getActivity(), R.layout.red_btn, null);
+    }
+
+    private void layoutGravity() {
+        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) testedLayoutGravityView.getLayoutParams();
+        layoutParams.gravity = Gravity.CENTER;
+        testedLayoutGravityView.setLayoutParams(layoutParams);
     }
 }
