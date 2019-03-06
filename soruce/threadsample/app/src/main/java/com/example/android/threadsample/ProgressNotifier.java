fb102154921f25bd1c33c606dfa69e791b1c0e67
Vicky <ying.vicky.cao@outlook.com>
2019-03-07 00:09:27 +0800

    Just add money，can not comple: images API can not be accessed

--- soruce/threadsample/app/src/main/java/com/example/android/threadsample/ProgressNotifier.java
+++ soruce/threadsample/app/src/main/java/com/example/android/threadsample/ProgressNotifier.java
@@ -1,5 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!--
+/*
  * Copyright (C) 2012 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,12 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
--->
-<!--
-    Defines a style resource for a black background
- -->
-<resources>
-    <style name="Theme.NoBackground" parent="@android:style/Theme.NoTitleBar">
-        <item name="android:windowBackground">@android:color/black</item>
-    </style>
-</resources>
+ */
+
+package com.example.android.threadsample;
+
+public interface ProgressNotifier {
+    public void notifyProgress(String paramString);
+}
