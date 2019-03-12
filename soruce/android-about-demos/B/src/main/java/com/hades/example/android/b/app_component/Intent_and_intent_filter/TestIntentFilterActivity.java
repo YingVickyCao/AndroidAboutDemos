b15477cb1d53980c652a55802a23fdd3f91105bd
Vicky <ying.vicky.cao@outlook.com>
2019-03-12 17:03:28 +0800

    refactor

--- soruce/android-about-demos/B/src/main/java/com/hades/example/android/b/app_component/Intent_and_intent_filter/TestIntentFilterActivity.java
+++ soruce/android-about-demos/B/src/main/java/com/hades/example/android/b/app_component/Intent_and_intent_filter/TestIntentFilterActivity.java
@@ -1,8 +1,9 @@
-package com.hades.example.android.b.Intent_and_intent_filter;
+package com.hades.example.android.b.app_component.Intent_and_intent_filter;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.TextView;
 
 import com.hades.example.android.b.R;
 
@@ -11,5 +12,8 @@ public class TestIntentFilterActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intent_filter);
+
+        String result = getIntent().getDataString();
+        ((TextView) findViewById(R.id.result)).setText(result);
     }
 }
--- soruce/android-about-demos/lib/src/main/java/com/hades/example/android/lib/utils/bitmap/CachingBitmaps.java
+++
@@ -1,9 +0,0 @@
-package com.hades.example.android.lib.utils.bitmap;
-
-import android.graphics.Bitmap;
-import android.util.LruCache;
-
-public class CachingBitmaps {
-    private LruCache<String, Bitmap> memoryCache;
-
-}
