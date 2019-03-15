e5269668256a659248b92cef8636b926f7f887d1
Vicky <ying.vicky.cao@outlook.com>
2019-03-15 15:55:21 +0800

    refactor

---
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/data_storage/database/DisplayDBFragment.java
@@ -0,0 +1,19 @@
+package com.hades.example.android.data_storage.database;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.hades.example.android.R;
+import com.hades.example.android.base.BaseFragment;
+
+public class DisplayDBFragment extends BaseFragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.data_storage_sqlite_display_db, container, false);
+        return view;
+    }
+}
---
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/data_storage/database/DummyItem.java
@@ -0,0 +1,4 @@
+package com.hades.example.android.data_storage.database;
+
+public class DummyItem {
+}
