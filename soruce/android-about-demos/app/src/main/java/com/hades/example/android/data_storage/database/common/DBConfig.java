7e48aeeb83a6738440480a9e10215726436b2531
Vicky <ying.vicky.cao@outlook.com>
2019-03-20 09:56:35 +0800

    refactor

--- soruce/android-about-demos/app/src/main/java/com/hades/example/android/data_storage/database/common/DBConfig.java
+++
@@ -1,6 +0,0 @@
-package com.hades.example.android.data_storage.database.common;
-
-public interface DBConfig {
-    public static final String DATABASE_FILE = "QmfDb"; // DB文件名
-    public static final int DB_VER = 1; //
-}
