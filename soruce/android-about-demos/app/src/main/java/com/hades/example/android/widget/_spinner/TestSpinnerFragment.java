c57a5bb1d2684463bfd043d137c116135f69bef6
Vicky <ying.vicky.cao@outlook.com>
2019-03-28 19:25:56 +0800

    refactor

--- soruce/android-about-demos/app/src/main/java/com/hades/example/android/widget/_spinner/TestSpinnerFragment.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/widget/_spinner/TestSpinnerFragment.java
@@ -29,9 +29,26 @@ public class TestSpinnerFragment extends BaseFragment {
         return view;
     }
 
+    /*
+     <style name="Widget.Spinner">
+        <item name="background">@drawable/btn_dropdown</item>
+        <item name="clickable">true</item>
+        <item name="spinnerMode">dialog</item>
+
+        <item name="dropDownSelector">@drawable/list_selector_background</item>
+        <item name="popupBackground">@drawable/spinner_dropdown_background</item>
+        <item name="dropDownVerticalOffset">-10dip</item>
+        <item name="dropDownHorizontalOffset">0dip</item>
+        <item name="dropDownWidth">wrap_content</item>
+        <item name="popupPromptView">@layout/simple_dropdown_hint</item>
+        <item name="gravity">center</item>
+    </style>
+
+     */
     private void test() {
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.widget_spinner_checked_text, gradeList) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, gradeList) {
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), R.layout.widget_spinner_checked_text, gradeList) {
 
             @Override
             public View getDropDownView(int position, View convertView, ViewGroup parent) {
