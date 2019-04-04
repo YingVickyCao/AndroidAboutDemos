e7cb02a972b9f262c1fc756f637461b271ca223c
Vicky <ying.vicky.cao@outlook.com>
2019-04-04 17:08:29 +0800

    Add react_native demos

---
+++ soruce/ReactNativeTestCode/js/_1_create_component/_1_es6/RedBtn.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+export default class RedBtn extends Component {
+    render() {
+        return (
+            <View>
+                <Text style={{color: 'green'}}>A world!</Text>
+            </View>
+        );
+    }
+}
