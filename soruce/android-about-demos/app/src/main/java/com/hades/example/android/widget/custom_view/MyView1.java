026178265ae110ec00154a1636624fa3c2c47fe3
Vicky <ying.vicky.cao@outlook.com>
2019-04-24 15:15:36 +0800

    custom_view

--- soruce/android-about-demos/app/src/main/java/com/hades/example/android/widget/custom_view/MyView1.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/widget/custom_view/MyView1.java
@@ -8,6 +8,7 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.Nullable;
 
 import com.hades.example.android.R;
@@ -35,12 +36,13 @@ public class MyView1 extends View {
 
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-//        paint.setColor(Color.RED); //设置画笔颜色
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            paint.setColor(getResources().getColor(R.color.dark_m_0_opacity_80, getContext().getTheme())); //设置画笔颜色
-        } else {
-            paint.setColor(getResources().getColor(R.color.dark_m_0_opacity_80)); //设置画笔颜色
-        }
+
+//        paint.setColor(getColor()); //设置画笔颜色
+
+        paint.setColor(getColor2()); //设置画笔颜色
+        paint.setAlpha((int) (0.8 * 255));
+
+
         paint.setStyle(Paint.Style.FILL);   //设置画笔为空心 如果将这里改为Style.STROKE  这个图中的实线圆柱体就变成了空心的圆柱体
 //        canvas.drawColor(Color.WHITE); //将背景填充成白色
         canvas.drawRect(0, 0, getSize(), getSize(), paint); //绘制矩形
@@ -48,5 +50,23 @@ public class MyView1 extends View {
 
     private int getSize() {
         return (int) getResources().getDimension(R.dimen.size_100);
+    }
+
+    @ColorInt
+    private int getColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return getResources().getColor(R.color.dark_m_0_opacity_80, getContext().getTheme());
+        } else {
+            return getResources().getColor(R.color.dark_m_0_opacity_80);
+        }
+    }
+
+    @ColorInt
+    private int getColor2() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return getResources().getColor(R.color.dark_m_0, getContext().getTheme());
+        } else {
+            return getResources().getColor(R.color.dark_m_0);
+        }
     }
 }
--- soruce/android-about-demos/app/src/main/java/com/hades/example/android/widget/custom_view/MyView2.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/widget/custom_view/MyView2.java
@@ -8,6 +8,7 @@ import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 
+import androidx.annotation.ColorInt;
 import androidx.annotation.Nullable;
 
 import com.hades.example.android.R;
@@ -15,7 +16,6 @@ import com.hades.example.android.R;
 @SuppressLint("AppCompatCustomView")
 public class MyView2 extends View {
     Paint paint = new Paint();
-
 
     public MyView2(Context context) {
         super(context);
@@ -35,18 +35,35 @@ public class MyView2 extends View {
 
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-//        paint.setColor(Color.RED); //设置画笔颜色
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            paint.setColor(getResources().getColor(R.color.light_m_0_opacity_30, getContext().getTheme())); //设置画笔颜色
-        } else {
-            paint.setColor(getResources().getColor(R.color.light_m_0_opacity_30)); //设置画笔颜色
-        }
+
+        paint.setColor(getColor()); //设置画笔颜色
+        paint.setAlpha((int) (0.3 * 255));
+
+//        paint.setColor(getColor2()); //设置画笔颜色
+
         paint.setStyle(Paint.Style.FILL);   //设置画笔为空心 如果将这里改为Style.STROKE  这个图中的实线圆柱体就变成了空心的圆柱体
-//        canvas.drawColor(Color.WHITE); //将背景填充成白色
         canvas.drawRect(0, 0, getSize(), getSize(), paint); //绘制矩形
     }
 
     private int getSize() {
         return (int) getResources().getDimension(R.dimen.size_100);
+    }
+
+    @ColorInt
+    private int getColor2() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return getResources().getColor(R.color.light_m_0_opacity_30, getContext().getTheme());
+        } else {
+            return getResources().getColor(R.color.light_m_0_opacity_30);
+        }
+    }
+
+    @ColorInt
+    private int getColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return getResources().getColor(R.color.light_m_0, getContext().getTheme());
+        } else {
+            return getResources().getColor(R.color.light_m_0);
+        }
     }
 }
