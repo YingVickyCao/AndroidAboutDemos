f6c1e26c366a93a964862f838f3ec1288e8910e4
Vicky <ying.vicky.cao@outlook.com>
2019-04-24 17:06:59 +0800

    add custom

--- soruce/android-about-demos/app/src/main/java/com/hades/example/android/widget/custom_view/MyView.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/widget/custom_view/MyView.java
@@ -2,10 +2,12 @@ package com.hades.example.android.widget.custom_view;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.ColorInt;
@@ -15,6 +17,7 @@ import com.hades.example.android.R;
 
 @SuppressLint("AppCompatCustomView")
 public class MyView extends View {
+    private static final String TAG = MyView.class.getSimpleName();
 
     private @ColorInt
     int colorId;
@@ -22,31 +25,34 @@ public class MyView extends View {
 
     public MyView(Context context) {
         super(context);
+        Log.d(TAG, "MyView: 1");
     }
 
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        Log.d(TAG, "MyView: 2");
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MyView, R.attr.MyViewStyle, R.style.MyView_Default);
+        if (null == typedArray) {
+            return;
+        }
+        colorId = typedArray.getColor(R.styleable.MyView_bgColor, Color.RED);
+        typedArray.recycle();
     }
 
     public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        Log.d(TAG, "MyView: 3");
     }
 
     public MyView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-//
-//        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MyView, R.attr.MyViewStyle, defStyleRes);
-//        if (null == typedArray) {
-//            return;
-//        }
-//        colorId = typedArray.getColor(R.styleable.MyView_bgColor, Color.RED);
-//        typedArray.recycle();
+        Log.d(TAG, "MyView: 4");
     }
 
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        paint.setColor(Color.RED); //设置画笔颜色
+        paint.setColor(colorId); //设置画笔颜色
 
         paint.setStyle(Paint.Style.FILL);   //设置画笔为空心 如果将这里改为Style.STROKE  这个图中的实线圆柱体就变成了空心的圆柱体
         canvas.drawRect(0, 0, getSize(), getSize(), paint); //绘制矩形
