2a840c4d4f227d115570eebdc68b91858cd6adda
Vicky <ying.vicky.cao@outlook.com>
2019-04-25 08:09:11 +0800

    String,Integer, and Arrays

--- soruce/android-about-demos/app/src/main/java/com/hades/example/android/resource/_array/TestStringIntegerArrayFragment.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/resource/_array/TestStringIntegerArrayFragment.java
@@ -4,6 +4,7 @@ import android.app.Fragment;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Annotation;
 import android.text.Html;
@@ -29,47 +30,72 @@ import com.hades.example.android.R;
 public class TestStringIntegerArrayFragment extends Fragment {
     private static final String TAG = TestStringIntegerArrayFragment.class.getSimpleName();
 
-    Button normalArrayBtn;
+    Button checkTypedArray;
+
+    TextView mTv_stylingWithHtmlMarkup;
+    TextView mTv_stylingWithSpannables;
+    TextView mTv_formattingStrings;
+    TextView mTv_stylingWithAnnotations;
+
     TextView quantityStringsResult;
-    TextView formattingStrings;
-    TextView stylingWithAnnotations;
-    TextView stylingWithAnnotations2;
+
+    Spannable spannable;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.res_string_integer_array, container, false);
-        normalArrayBtn = view.findViewById(R.id.getNormalArray);
+
+        mTv_formattingStrings = view.findViewById(R.id.formattingStrings);
+        mTv_stylingWithHtmlMarkup = view.findViewById(R.id.styling_with_html_markup);
+        mTv_stylingWithSpannables = view.findViewById(R.id.styling_with_spannables);
+        mTv_stylingWithAnnotations = view.findViewById(R.id.stylingWithAnnotations);
+
         quantityStringsResult = view.findViewById(R.id.quantityStringsResult);
-        formattingStrings = view.findViewById(R.id.formattingStrings);
-        stylingWithAnnotations = view.findViewById(R.id.stylingWithAnnotations);
-        stylingWithAnnotations2 = view.findViewById(R.id.stylingWithAnnotations2);
 
-        format1(view);
-        format2(view);
+        checkTypedArray = view.findViewById(R.id.checkTypedArray);
 
-        normalArrayBtn.setOnClickListener(v -> getNormalArray());
-
+        checkTypedArray.setOnClickListener(v -> checkTypedArray());
         view.findViewById(R.id.getStringArray).setOnClickListener(v -> getStringArray());
         view.findViewById(R.id.quantityStrings).setOnClickListener(v -> quantityStrings());
-
         view.findViewById(R.id.getIntArray).setOnClickListener(v -> getIntArray());
 
-        formattingStrings.setText(getString(R.string.formatting_strings, "A", 10));
-        stylingWithAnnotations.setText(getStylingWithAnnotations());
+        mTv_formattingStrings.setText(getString(R.string.formatting_strings, "A", 10));
 
-//        stylingWithAnnotations2.setText(getStylingWithAnnotations2());
+        mTv_stylingWithHtmlMarkup.setText(Html.fromHtml(getString(R.string.styling_with_html_markup)));
+        mTv_stylingWithHtmlMarkup.setMovementMethod(LinkMovementMethod.getInstance());
+
+        spannable = new SpannableString(getString(R.string.styling_with_spannables));
+        spannable.setSpan(new BackgroundColorSpan(Color.RED), 1, 4, 0);
+        spannable.setSpan(new ForegroundColorSpan(Color.BLUE), 5, 9, 0);
+        mTv_stylingWithSpannables.setText(spannable);
+
+        mTv_stylingWithAnnotations.setText(getmTv_stylingWithAnnotations());
 
         return view;
     }
 
-    private void getNormalArray() {
-        TypedArray typedArray = getResources().obtainTypedArray(R.array.normal_array);
-//        Drawable drawable = typedArray.getDrawable(0);
-//        normalArrayBtn.setBackground(drawable);
+    // TODO: 2019-04-24
+    private SpannableString getmTv_stylingWithAnnotations() {
+        SpannableString spannableString = new SpannableString("Styling with annotations");
+        Annotation annotation = new Annotation("font", "title_emphasis");
+        spannableString.setSpan(annotation, 3, 7, 33);
+        return spannableString;
+    }
 
-        int color = typedArray.getColor(1, 0);
-        normalArrayBtn.setBackgroundColor(color);
+    private void checkTypedArray() {
+        Resources res = getResources();
+        TypedArray icons = res.obtainTypedArray(R.array.icons);
+        Drawable drawable = icons.getDrawable(0);
+        icons.recycle();
+
+
+        TypedArray colors = res.obtainTypedArray(R.array.colors);
+        int color = colors.getColor(0, 0);
+        colors.recycle();
+
+        checkTypedArray.setBackground(drawable);
+        checkTypedArray.setTextColor(color);
     }
 
     private void quantityStrings() {
@@ -82,8 +108,7 @@ public class TestStringIntegerArrayFragment extends Fragment {
     }
 
     private String getQuantityStrings(int count) {
-        Resources res = getResources();
-        return res.getQuantityString(R.plurals.numberOfSongsAvailable, count, count);
+        return getResources().getQuantityString(R.plurals.numberOfSongsAvailable, count, count);
     }
 
     private void getStringArray() {
@@ -108,35 +133,5 @@ public class TestStringIntegerArrayFragment extends Fragment {
             str.append(ints[i]).append(",");
         }
         Log.d(TAG, "getIntArray: " + str);
-    }
-
-
-    private void format1(View view) {
-        final TextView textView1 = view.findViewById(R.id.my_text_view_html);
-        textView1.setText(Html.fromHtml(getString(R.string.styling_with_html_markup)));
-        textView1.setMovementMethod(LinkMovementMethod.getInstance());
-    }
-
-    private void format2(View view) {
-        final Spannable text2 = new SpannableString(getString(R.string.styling_with_spannables));
-        text2.setSpan(new BackgroundColorSpan(Color.RED), 1, 4, 0);
-        text2.setSpan(new ForegroundColorSpan(Color.BLUE), 5, 9, 0);
-        ((TextView) view.findViewById(R.id.my_text_view_spannable)).setText(text2);
-    }
-
-    // TODO: 2019-04-24
-    private SpannableString getStylingWithAnnotations() {
-        SpannableString spannableString = new SpannableString("My spantastic text");
-        Annotation annotation = new Annotation("font", "title_emphasis");
-        spannableString.setSpan(annotation, 3, 7, 33);
-        return spannableString;
-    }
-
-    // TODO: 2019-04-24
-    private SpannableString getStylingWithAnnotations2() {
-        SpannableString spannableString = (SpannableString) getText(R.string.title);
-        Annotation annotation = new Annotation("font", "title_emphasis");
-        spannableString.setSpan(annotation, 3, 7, 33);
-        return spannableString;
     }
 }
