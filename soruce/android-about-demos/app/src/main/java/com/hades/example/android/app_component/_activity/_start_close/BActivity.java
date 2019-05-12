ce7f04b73218ba2f68ebaca6a741728caa7b31f7
Vicky <ying.vicky.cao@outlook.com>
2019-05-12 16:03:35 +0800

    Activity: start, finish, transfer data

--- soruce/android-about-demos/app/src/main/java/com/hades/example/android/app_component/_activity/_start_close/BActivity.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/app_component/_activity/_start_close/BActivity.java
@@ -2,13 +2,19 @@ package com.hades.example.android.app_component._activity._start_close;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.hades.example.android.R;
 
+import static com.hades.example.android.app_component._activity._start_close.AActivity.KEY_A;
+
 public class BActivity extends AppCompatActivity {
+
+    public final static String KEY_B = "B";
+    public final static int KEY_B_RESULT_CODE = 2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -17,16 +23,69 @@ public class BActivity extends AppCompatActivity {
         setContentView(R.layout.activity_b);
 
         findViewById(R.id.pageA).setOnClickListener(v -> pageA());
+        findViewById(R.id.jumpThemFinish).setOnClickListener(v -> jumpThemFinish());
         findViewById(R.id.close).setOnClickListener(v -> close());
+
+        findViewById(R.id.returnResult).setOnClickListener(v -> returnResult());
+
+        Intent intent = getIntent();
+        long data = intent.getLongExtra(KEY_A, 0);
+        runOnUiThread(() -> Toast.makeText(BActivity.this, String.valueOf(data), Toast.LENGTH_SHORT).show());
     }
 
+    /*
+     Back Stack:
+     A
+     B
+
+     ->
+
+     A
+     B
+     A
+     */
+    // Depressed:
     private void pageA() {
         Intent intent = new Intent(this, AActivity.class);
         startActivity(intent);
     }
 
-    private void close() {
+    /*
+     Back Stack:
+     A
+     B
+
+     ->
+
+     A
+     A
+     */
+    // Depressed:
+    private void jumpThemFinish() {
         pageA();
+        close();
+    }
+
+    /*
+    Back Stack:
+    A
+    B
+
+    ->
+
+    A
+    */
+    // Recommended:
+    private void close() {
         finish();
+    }
+
+    private void returnResult() {
+        Intent intent = getIntent();
+        intent.putExtra(KEY_B, 100);
+        setResult(KEY_B_RESULT_CODE, intent);
+
+        finish();
+//        finishActivity(KEY_A_REQUEST_CODE);
     }
 }
