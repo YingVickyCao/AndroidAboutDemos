2dd2476ab9215d4e86303adba4c66ab18a1637ed
Vicky <ying.vicky.cao@outlook.com>
2019-05-14 21:49:43 +0800

    Intent-Filter : action , category

---
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/app_component/_intent_and_intent_filter/_action_category/B.java
@@ -0,0 +1,34 @@
+package com.hades.example.android.app_component._intent_and_intent_filter._action_category;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hades.example.android.R;
+
+public class B extends AppCompatActivity {
+    private static final String TAG = B.class.getSimpleName();
+
+    public static final String ACTION_1 = B.class.getCanonicalName() + "." + "ACTION_1";
+    public static final String ACTION_2 = B.class.getCanonicalName() + "." + "ACTION_2";
+
+    public static final String CATEGORY_1 = B.class.getCanonicalName() + "." + "CATEGORY_1";
+    public static final String CATEGORY_2 = B.class.getCanonicalName() + "." + "CATEGORY_2";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.intent_action_and_category_b);
+
+        TextView show = findViewById(R.id.show);
+        Intent intent = getIntent();
+        String str = "action：" + intent.getAction() + "\nCategories：" + intent.getCategories();
+        Log.d(TAG, "onCreate: " + str);
+        show.setText(str);
+    }
+}
---
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/app_component/_intent_and_intent_filter/_action_category/C.java
@@ -0,0 +1,34 @@
+package com.hades.example.android.app_component._intent_and_intent_filter._action_category;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hades.example.android.R;
+
+public class C extends AppCompatActivity {
+    private static final String TAG = C.class.getSimpleName();
+
+    public static final String ACTION_3 = C.class.getCanonicalName() + "." + "ACTION_3";
+    public static final String CATEGORY_3 = C.class.getCanonicalName() + "." + "CATEGORY_3";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.intent_action_and_category_b);
+
+        TextView page = findViewById(R.id.page);
+        page.setBackgroundColor(getResources().getColor(android.R.color.holo_blue_bright));
+
+        TextView show = findViewById(R.id.show);
+        Intent intent = getIntent();
+        String str = "action：" + intent.getAction() + "\nCategories：" + intent.getCategories();
+        Log.d(TAG, "onCreate: " + str);
+        show.setText(str);
+    }
+}
