5a10030f52eb1beb2fbe3680668f6ab853d5407c
Vicky <ying.vicky.cao@outlook.com>
2019-05-17 08:07:49 +0800

    Intent-Filter_Data_Type.jpg

--- soruce/android-about-demos/app/src/main/java/com/hades/example/android/app_component/_intent_and_intent_filter/_data/A.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/app_component/_intent_and_intent_filter/_data/A.java
@@ -24,8 +24,13 @@ public class A extends AppCompatActivity {
         findViewById(R.id.case3).setOnClickListener(v -> case3());
         findViewById(R.id.case4).setOnClickListener(v -> case4());
         findViewById(R.id.case5).setOnClickListener(v -> case5());
+        findViewById(R.id.case6).setOnClickListener(v -> case6());
+        findViewById(R.id.case7).setOnClickListener(v -> case7());
+        findViewById(R.id.case8).setOnClickListener(v -> case8());
+        findViewById(R.id.case9).setOnClickListener(v -> case9());
     }
 
+    // Intent { dat=scheme1: }
     private void case1() {
         Intent intent = new Intent();
         Uri uri = Uri.parse("scheme1:");
@@ -34,51 +39,89 @@ public class A extends AppCompatActivity {
 //        intent.addCategory(Intent.CATEGORY_DEFAULT);
 //        intent.setAction("action1");
 
-        Log.d(TAG, "scheme_category: " + uri.toString());
+        Log.d(TAG, "case1: " + intent.toString());
         startActivity(intent);
     }
 
+    // Intent { dat=scheme2://com.hades }
     private void case2() {
+        Intent intent = new Intent();
+        Uri uri = Uri.parse("scheme2://com.hades");
+        intent.setData(uri);
+        Log.d(TAG, "case2: " + intent.toString());
+        startActivity(intent);
+    }
+
+    // ERROR:android.content.ActivityNotFoundException: No Activity found to handle Intent { act=action1 cat=[android.intent.category.DEFAULT] dat=scheme2://com.hades }
+    private void case3() {
         Intent intent = new Intent();
         Uri uri = Uri.parse("scheme2://com.hades");
         intent.setData(uri);
         intent.addCategory(Intent.CATEGORY_DEFAULT);
         intent.setAction("action1");
 
-        Log.d(TAG, "scheme_category: " + uri.toString());
+        Log.d(TAG, "case3: " + intent.toString());
         startActivity(intent);
     }
 
-    private void case3() {
+    private void case4() {
         Intent intent = new Intent();
         Uri uri = Uri.parse("scheme2://com.hades");
         intent.setData(uri);
         intent.addCategory(Intent.CATEGORY_DEFAULT);
         intent.setAction("action2");
 
-        Log.d(TAG, "scheme_category: " + uri.toString());
+        Log.d(TAG, "case4: " + intent.toString());
         startActivity(intent);
     }
 
-    private void case4() {
+    private void case5() {
         Intent intent = new Intent();
         Uri uri = Uri.parse("scheme2://com.hades:1000/path2");
         intent.setData(uri);
         intent.addCategory(Intent.CATEGORY_DEFAULT);
         intent.setAction("action2");
 
-        Log.d(TAG, "scheme_category: " + uri.toString());
+        Log.d(TAG, "case5: " + intent.toString());
         startActivity(intent);
     }
 
-    private void case5() {
+    private void case6() {
         Intent intent = new Intent();
         Uri uri = Uri.parse("scheme2:/com.hades:1000/path2");
         intent.setData(uri);
         intent.addCategory(Intent.CATEGORY_DEFAULT);
         intent.setAction("action2");
 
-        Log.d(TAG, "scheme_category: " + uri.toString());
+        Log.d(TAG, "case6: " + intent.toString());
+        startActivity(intent);
+    }
+
+    private void case7() {
+        Intent intent = new Intent();
+        Uri uri = Uri.parse("scheme3://com.hades/path1");
+        intent.setType(getString(R.string.mime_type_1));
+        intent.setData(uri);
+
+        Log.d(TAG, "case7: " + intent.toString());
+        startActivity(intent);
+    }
+
+    // Intent { typ=a/b }
+    private void case8() {
+        Intent intent = new Intent();
+        Uri uri = Uri.parse("scheme3://com.hades/path1");
+        intent.setData(uri);
+        intent.setType(getString(R.string.mime_type_1));
+        Log.d(TAG, "case8: " + intent.toString());
+        startActivity(intent);
+    }
+
+    private void case9() {
+        Intent intent = new Intent();
+        Uri uri = Uri.parse("scheme3://com.hades/path2");
+        intent.setDataAndType(uri, getString(R.string.mime_type_1));
+        Log.d(TAG, "case9: " + intent.toString());
         startActivity(intent);
     }
 }
