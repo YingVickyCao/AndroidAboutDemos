d5544ed2d4c986e330e9c3fd8ef08ce1c31ada6e
Vicky <ying.vicky.cao@outlook.com>
2019-05-27 20:09:41 +0800

    RectWithBorder

---
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/widget/custom_view/RectWithBorder.java
@@ -0,0 +1,43 @@
+package com.hades.example.android.widget.custom_view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import com.hades.example.android.R;
+
+public class RectWithBorder extends View {
+    private static final String TAG = RectWithBorder.class.getSimpleName();
+
+    Paint paint;
+
+    public RectWithBorder(Context context, AttributeSet set) {
+        super(context, set);
+        paint = new Paint();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+
+        Log.d(TAG, "draw:" + "left=" + getLeft() + ",top=" + getTop() + ",right=" + getRight() + ",bottom=" + getBottom());
+
+        canvas.drawColor(Color.BLACK);
+
+        paint.setAntiAlias(true);
+        paint.setColor(Color.GREEN);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(getResources().getDimension(R.dimen.size_2));
+
+        int left = (int) getResources().getDimension(R.dimen.size_50);
+        int top = (int) getResources().getDimension(R.dimen.size_25);
+        int right = (int) getResources().getDimension(R.dimen.size_150);
+        int bottom = (int) getResources().getDimension(R.dimen.size_75);
+        int radius = (int) getResources().getDimension(R.dimen.size_1);
+        canvas.drawRoundRect(left, top, right, bottom, radius, radius, paint);
+    }
+}
