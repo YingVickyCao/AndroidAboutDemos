d1c64833a48506b201a5ac5eb733da996dc36ab3
Vicky <ying.vicky.cao@outlook.com>
2019-05-29 12:29:47 +0800

    PathEffect

--- soruce/android-about-demos/app/src/main/java/com/hades/example/android/widget/custom_view/TestCustomViewFragment.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/widget/custom_view/TestCustomViewFragment.java
@@ -19,7 +19,8 @@ public class TestCustomViewFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.custom_view, container, false);
+        View view = inflater.inflate(R.layout.custom_view_path, container, false);
+//        View view = inflater.inflate(R.layout.custom_view, container, false);
 //        View view = inflater.inflate(R.layout.custom_view_canvas_left_position_1, container, false);
 //        View view = inflater.inflate(R.layout.custom_view_canvas_left_position_2, container, false);
         getScreenDensity();
