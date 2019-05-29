510e1dca660162bc4353abaf3cfdd21d05d22c5a
Vicky <ying.vicky.cao@outlook.com>
2019-05-29 08:35:23 +0800

    Line

---
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/widget/custom_view/Line.java
@@ -0,0 +1,41 @@
+package com.hades.example.android.widget.custom_view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+import com.hades.example.android.R;
+
+public class Line extends View {
+    private static final String TAG = Line.class.getSimpleName();
+
+    Paint paint;
+
+    public Line(Context context, AttributeSet set) {
+        super(context, set);
+        paint = new Paint();
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+
+        Log.d(TAG, "draw:" + "left=" + getLeft() + ",top=" + getTop() + ",right=" + getRight() + ",bottom=" + getBottom());
+
+        canvas.drawColor(Color.BLACK);
+
+        paint.setAntiAlias(true);
+        paint.setColor(Color.GREEN);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(getResources().getDimension(R.dimen.size_2));
+
+        canvas.drawLine((int) getResources().getDimension(R.dimen.size_50)
+                , (int) getResources().getDimension(R.dimen.size_50)
+                , (int) getResources().getDimension(R.dimen.size_150)
+                , (int) getResources().getDimension(R.dimen.size_50), paint);
+    }
+}
