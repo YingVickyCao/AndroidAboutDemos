de20bb9b3f71ff724e0faa4cbef6f8f830c625f8
Vicky <ying.vicky.cao@outlook.com>
2019-06-04 09:34:25 +0800

    Update CustomNestedScrollView.java

--- soruce/TestNav/app/src/main/java/com/example/android/widget/list/dag_reorder_list2/v2/CustomNestedScrollView.java
+++ soruce/TestNav/app/src/main/java/com/example/android/widget/list/dag_reorder_list2/v2/CustomNestedScrollView.java
@@ -2,8 +2,6 @@ package com.example.android.widget.list.dag_reorder_list2.v2;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,14 +22,43 @@ public class CustomNestedScrollView extends NestedScrollView {
         super(context, attrs, defStyleAttr);
     }
 
+    /*
     @Override
     public void onNestedPreScroll(View target, int dx, int dy, int[] consumed) {
+        Log.d(TAG, "onNestedPreScroll: dx=" + dx + ",dy=" + dy);
+
+        final RecyclerView rv = (RecyclerView) target;
+        if ((dy < 0 && isRvScrolledToTop(rv)) || (dy > 0 && !isNsvScrolledToBottom(this))) {
+            scrollBy(0, dy);
+            consumed[1] = dy;
+            return;
+        }
         super.onNestedPreScroll(target, dx, dy, consumed);
-        Log.d(TAG, "onNestedPreScroll: ");
+    }
+
+    @Override
+    public boolean onNestedPreFling(View target, float velocityX, float velocityY) {
+        Log.d(TAG, "onNestedPreScroll: velocityX=" + velocityX + ",dy=" + velocityY);
+
+        final RecyclerView rv = (RecyclerView) target;
+        if ((velocityY < 0 && isRvScrolledToTop(rv)) || (velocityY > 0 && !isNsvScrolledToBottom(this))) {
+            fling((int) velocityY);
+            return true;
+        }
+        return super.onNestedPreFling(target, velocityX, velocityY);
     }
 
     @Override
     public boolean onNestedFling(View target, float velocityX, float velocityY, boolean consumed) {
         return super.onNestedFling(target, velocityX, velocityY, consumed);
     }
+
+    private static boolean isNsvScrolledToBottom(NestedScrollView nsv) {
+        return !nsv.canScrollVertically(1);
+    }
+
+    private static boolean isRvScrolledToTop(RecyclerView rv) {
+        final LinearLayoutManager lm = (LinearLayoutManager) rv.getLayoutManager();
+        return lm.findLastVisibleItemPosition() == 0 && lm.findViewByPosition(0).getTop() == 0;
+    }*/
 }
