9eaca56f16e17a4ea1bb0504b68409c1740c1ce0
Vicky <ying.vicky.cao@outlook.com>
2019-07-05 17:53:12 +0800

    Update DragAndReorderListFragment.java

--- soruce/android-about-demos/app/src/main/java/com/hades/example/android/widget/_list/_recyclerview/_dag_reorder_list/v2/DragAndReorderListFragment.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/widget/_list/_recyclerview/_dag_reorder_list/v2/DragAndReorderListFragment.java
@@ -64,114 +64,14 @@ public class DragAndReorderListFragment extends Fragment {
         RecyclerView rv1 = view.findViewById(R.id.lv1);
         RecyclerView rv2 = view.findViewById(R.id.lv2);
 
-//        rv1.setLayoutManager(getLinearLayoutManager());
-//        rv1.setHasFixedSize(true);// PO
-//        ItemTouchHelperAdapter adapter1 = new ItemTouchHelperAdapter(list1, getActivity());
-//        adapter1.setGroupResId(R.layout.widget_recyclerview_4_drag_reorder_item_view_v2_1);
-//        adapter1.setOpenedPage(mOpenedPage);
-//        IDragView startDragListener1 = new IDragView() {
-//
-//            @Override
-//            public void startDrag(RecyclerView.ViewHolder viewHolder) {
-//                Log.d(TAG, "startDrag: group0 height=" + group0.getMeasuredHeight());
-//                getActivity().runOnUiThread(() -> mItemTouchHelper1.startDrag(viewHolder));
-//            }
-//
-//            @Override
-//            public void endDrag() {
-//                adapter1.resetIsOnDragTag();
-//                hideLoading();
-//            }
-//
-//            @Override
-//            public void startSwipe(RecyclerView.ViewHolder viewHolder) {
-//                mItemTouchHelper1.startSwipe(viewHolder);
-//            }
-//
-//            @Override
-//            public void hideLoading() {
-//                DragAndReorderListFragment.this.hideLoading();
-//            }
-//
-//            @Override
-//            public void showLoading() {
-//                DragAndReorderListFragment.this.showLoading();
-//            }
-//
-//            @Override
-//            public void openPage(Group bean, boolean isGroup, String title, String childTitle) {
-//                String openedCurrentPage = "Open " + (isGroup ? title : childTitle);
-//                Toast.makeText(getContext(), openedCurrentPage, Toast.LENGTH_SHORT).show();
-//
-//                mOpenedPage.updateOpenedPage(title, childTitle, "Group1", isGroup);
-//                mCurrentPageView.setText(openedCurrentPage);
-//                adapter1.notifyDataSetChanged();
-//            }
-//        };
-//        adapter1.setDragView(startDragListener1);
-//        adapter1.setHasStableIds(true);
-//        rv1.setAdapter(adapter1);
-//        mItemTouchHelper1 = new ItemTouchHelper(new SimpleItemTouchHelperCallback(adapter1, startDragListener1, rv1, nestedScrollView));
-//        mItemTouchHelper1.attachToRecyclerView(rv1);
-
         SimpleItemTouchHelperCallback simpleItemTouchHelperCallback = new SimpleItemTouchHelperCallback();
         SimpleItemTouchHelperCallback simpleItemTouchHelperCallback2 = new SimpleItemTouchHelperCallback();
         mItemTouchHelper1 = new ItemTouchHelper(simpleItemTouchHelperCallback);
         mItemTouchHelper2 = new ItemTouchHelper(simpleItemTouchHelperCallback2);
         setViews(rv1, list1, mItemTouchHelper1, simpleItemTouchHelperCallback);
         setViews(rv2, list2, mItemTouchHelper2, simpleItemTouchHelperCallback2);
-
-//        rv2.setLayoutManager(getLinearLayoutManager());
-//        rv2.setHasFixedSize(true);// PO
-//        ItemTouchHelperAdapter adapter2 = new ItemTouchHelperAdapter(list2, getActivity());
-//        adapter2.setGroupResId(R.layout.widget_recyclerview_4_drag_reorder_item_view_v2_2);
-//        adapter2.setOpenedPage(mOpenedPage);
-//        IDragView startDragListener2 = new IDragView() {
-//            @Override
-//            public void startDrag(RecyclerView.ViewHolder viewHolder) {
-//                getActivity().runOnUiThread(() -> mItemTouchHelper2.startDrag(viewHolder));
-//            }
-//
-//            @Override
-//            public void endDrag() {
-//                adapter2.resetIsOnDragTag();
-//                hideLoading();
-//            }
-//
-//            @Override
-//            public void startSwipe(RecyclerView.ViewHolder viewHolder) {
-//                mItemTouchHelper2.startSwipe(viewHolder);
-//            }
-//
-//            @Override
-//            public void hideLoading() {
-//                DragAndReorderListFragment.this.hideLoading();
-//            }
-//
-//            @Override
-//            public void showLoading() {
-//                DragAndReorderListFragment.this.showLoading();
-//            }
-//
-//            @Override
-//            public void openPage(Group bean, boolean isGroup, String title, String childTitle) {
-//                Toast.makeText(getContext(), "Open " + bean.getTitle(), Toast.LENGTH_SHORT).show();
-//                mOpenedPage.updateOpenedPage(title, childTitle, "Group2", isGroup);
-//                adapter2.notifyDataSetChanged();
-//            }
-//        };
-//        adapter2.setDragView(startDragListener2);
-//        adapter2.setHasStableIds(true);
-//        rv2.setAdapter(adapter2);
-//        mItemTouchHelper2 = new ItemTouchHelper(new SimpleItemTouchHelperCallback(adapter2, startDragListener2, rv2, nestedScrollView));
-//        mItemTouchHelper2.attachToRecyclerView(rv2);
-
-//        mCurrentPageView.setFocusable(true);
-//        mCurrentPageView.setFocusableInTouchMode(true);
-//        mCurrentPageView.requestFocus();
         return view;
     }
-
 
     private void setViews(RecyclerView rv, List<Group> list1, ItemTouchHelper itemTouchHelper, SimpleItemTouchHelperCallback callback) {
         rv.setLayoutManager(getLinearLayoutManager());
