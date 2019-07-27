4e91074dbef53057064dd6ca5d04f0919121b569
Vicky <ying.vicky.cao@outlook.com>
2019-07-27 14:50:22 +0800

    磁场传感器(magnetic field)

--- soruce/android-about-demos/app/src/main/java/com/hades/example/android/sensor/TestSensorActivity.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/sensor/TestSensorActivity.java
@@ -62,9 +62,8 @@ public class TestSensorActivity extends Activity implements SensorEventListener 
 //        registerListener_TYPE_ACCELEROMETER();    // 加速度传感器
 //        registerListener_TYPE_ORIENTATION();      // 方位传感器
 //        registerListener_TYPE_ORIENTATION2();     // 方位传感器
-        registerListener_TYPE_GYROSCOPE();          // 螺仪传感器
-//        // 为系统的磁场传感器注册监听器
-//        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_GAME);
+//        registerListener_TYPE_GYROSCOPE();        // 螺仪传感器
+        registerListener_TYPE_MAGNETIC_FIELD();     // 磁场传感器
 //        // 为系统的重力传感器注册监听器
 //        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY), SensorManager.SENSOR_DELAY_GAME);
 //        // 为系统的线性加速度传感器注册监听器
@@ -129,6 +128,10 @@ public class TestSensorActivity extends Activity implements SensorEventListener 
         mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE), SensorManager.SENSOR_DELAY_NORMAL);
     }
 
+    private void registerListener_TYPE_MAGNETIC_FIELD() {
+        mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_GAME);
+    }
+
     @Override
     protected void onPause() {
         // 程序暂停时取消注册传感器监听器
@@ -170,15 +173,9 @@ public class TestSensorActivity extends Activity implements SensorEventListener 
 
             // 磁场传感器
             case Sensor.TYPE_MAGNETIC_FIELD:
-                sb = new StringBuilder();
-                sb.append("X轴方向上的磁场强度：");
-                sb.append(values[0]);
-                sb.append("\nY轴方向上的磁场强度：");
-                sb.append(values[1]);
-                sb.append("\nZ轴方向上的磁场强度：");
-                sb.append(values[2]);
-                etMagnetic.setText(sb.toString());
+                onSensorChanged_TYPE_MAGNETIC_FIELD(event);
                 break;
+
             // 重力传感器
             case Sensor.TYPE_GRAVITY:
                 sb = new StringBuilder();
@@ -247,8 +244,7 @@ public class TestSensorActivity extends Activity implements SensorEventListener 
 
     private void onSensorChanged_TYPE_ORIENTATION(float[] values, TextView tv) {
         // 获取触发event的传感器类型
-        StringBuilder sb;
-        sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         sb.append("绕Z轴转过的角度：");
         sb.append(values[0]);
         sb.append("\n绕X轴转过的角度：");
@@ -260,8 +256,7 @@ public class TestSensorActivity extends Activity implements SensorEventListener 
 
     private void onSensorChanged_TYPE_GYROSCOPE(SensorEvent event) {
         float[] values = event.values;
-        StringBuilder sb;
-        sb = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
         sb.append("绕X轴旋转的角速度：");
         sb.append(values[0]);
         sb.append("\n绕Y轴旋转的角速度：");
@@ -270,6 +265,20 @@ public class TestSensorActivity extends Activity implements SensorEventListener 
         sb.append(values[2]);
         etGyro.setText(sb.toString());
     }
+
+    private void onSensorChanged_TYPE_MAGNETIC_FIELD(SensorEvent event) {
+        float[] values = event.values;
+        StringBuilder sb;
+        sb = new StringBuilder();
+        sb.append("X轴方向上的磁场强度：");
+        sb.append(values[0]);
+        sb.append("\nY轴方向上的磁场强度：");
+        sb.append(values[1]);
+        sb.append("\nZ轴方向上的磁场强度：");
+        sb.append(values[2]);
+        etMagnetic.setText(sb.toString());
+    }
+
 
     public void onAccuracyChanged4Accelerometer(Sensor sensor, int accuracy) {
         Log.d(TAG, "onAccuracyChanged: accuracy=" + accuracy);
