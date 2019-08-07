117f6e529f56b388c1f86437d692685fb883c53c
Vicky <ying.vicky.cao@outlook.com>
2019-08-07 21:23:16 +0800

    Update TestManagerPhoneDesktopActivity.java

--- soruce/android-about-demos/app/src/main/java/com/hades/example/android/manager_phone_desktop/TestManagerPhoneDesktopActivity.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/manager_phone_desktop/TestManagerPhoneDesktopActivity.java
@@ -1,15 +1,14 @@
 package com.hades.example.android.manager_phone_desktop;
 
-import android.Manifest;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
 import com.hades.example.android.R;
-import com.hades.example.android.lib.base.RxPermissionsActivity;
+import com.hades.example.android.lib.base.BaseActivity;
 import com.hades.example.android.manager_phone_desktop.static_wallpaper.TestStaticWallpaperFragment;
 
-public class TestManagerPhoneDesktopActivity extends RxPermissionsActivity {
+public class TestManagerPhoneDesktopActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,11 +17,6 @@ public class TestManagerPhoneDesktopActivity extends RxPermissionsActivity {
         initViews();
 
         findViewById(R.id.page_SetStaticWallpaper).setOnClickListener(v -> page_SetStaticWallpaper());
-    }
-
-    @Override
-    protected void requestPermission() {
-        checkPermission("Request permission for Set Wallpaper", Manifest.permission.SET_WALLPAPER);
     }
 
     private void page_SetStaticWallpaper() {
