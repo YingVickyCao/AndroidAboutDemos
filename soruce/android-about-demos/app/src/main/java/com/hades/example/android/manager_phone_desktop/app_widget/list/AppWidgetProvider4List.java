0c4406d56561d234adc7993780db557a1ff1c2e3
Vicky <ying.vicky.cao@outlook.com>
2019-08-08 20:48:59 +0800

    refactor

---
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/manager_phone_desktop/app_widget/list/AppWidgetProvider4List.java
@@ -0,0 +1,57 @@
+package com.hades.example.android.manager_phone_desktop.app_widget.list;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import com.hades.example.android.R;
+
+public class AppWidgetProvider4List extends AppWidgetProvider {
+    private static final String TAG = AppWidgetProvider4List.class.getSimpleName();
+
+    public static final String TOAST_ACTION = AppWidgetProvider4List.class.getCanonicalName() + ".TOAST_ACTION";
+    public static final String EXTRA_ITEM = AppWidgetProvider4List.class.getCanonicalName() + ".EXTRA_ITEM";
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.app_widget_list_layout);
+        Intent intent = new Intent(context, RemoteViewsService4List.class);
+
+        // 使用intent更新rv中的stack_view组件（StackView）
+        rv.setRemoteAdapter(R.id.stack_view, intent);  // ①
+
+        // 设置当StackWidgetService提供的列表项为空时，直接显示empty_view组件
+        rv.setEmptyView(R.id.stack_view, R.id.empty_view);
+        // 创建启动StackWidgetProvider组件（作为BroadcastReceiver）的Intent
+
+        Intent toastIntent = new Intent(context, AppWidgetProvider4List.class);
+        // 为该Intent设置Action属性
+        toastIntent.setAction(AppWidgetProvider4List.TOAST_ACTION);
+        // 将Intent包装成PendingIntent
+        PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // 将PendingIntent与stack_view进行关联
+        rv.setPendingIntentTemplate(R.id.stack_view, toastPendingIntent);
+        // 使用AppWidgetManager通过RemteViews更新AppWidgetProvider
+        appWidgetManager.updateAppWidget(new ComponentName(context, AppWidgetProvider4List.class), rv); // ②
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+
+    // 重写该方法，将该组件当成BroadcastReceiver使用
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (TOAST_ACTION.equals(intent.getAction())) {
+            // 获取Intent中的数据
+            int viewIndex = intent.getIntExtra(EXTRA_ITEM, 0);
+            // 显示Toast提示
+            Toast.makeText(context, "点击第【" + viewIndex + "】个列表项", Toast.LENGTH_SHORT).show();
+        }
+        super.onReceive(context, intent);
+    }
+}
+
--- soruce/android-about-demos/app/src/main/java/com/hades/example/android/manager_phone_desktop/app_widget/list/RemoteViewsService4List.java
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/manager_phone_desktop/app_widget/list/RemoteViewsService4List.java
@@ -7,18 +7,16 @@ import android.widget.RemoteViewsService;
 
 import com.hades.example.android.R;
 
-public class StackWidgetService extends RemoteViewsService {
+public class RemoteViewsService4List extends RemoteViewsService {
     // 重写该方法，该方法返回一个RemoteViewsFactory对象
     // RemoteViewsFactory对象的作用类似于Adapter
     // 它负责为RemoteView中的指定组件提供多个列表项
     @Override
     public RemoteViewsFactory onGetViewFactory(Intent intent) {
-        return new StackRemoteViewsFactory(this.getApplicationContext(),
-                intent);  // ①
+        return new StackRemoteViewsFactory(this.getApplicationContext(), intent);  // ①
     }
 
-    class StackRemoteViewsFactory implements
-            RemoteViewsFactory {
+    class StackRemoteViewsFactory implements RemoteViewsFactory {
         // 定义一个数组来保存该组件生成的多个列表项
         private int[] items = null;
         private Context mContext;
@@ -29,13 +27,7 @@ public class StackWidgetService extends RemoteViewsService {
 
         @Override
         public void onCreate() {
-            // 初始化items数组
-            items = new int[]{R.drawable.bomb5, R.drawable.bomb6,
-                    R.drawable.bomb7, R.drawable.bomb8, R.drawable.bomb9,
-                    R.drawable.bomb10, R.drawable.bomb11, R.drawable.bomb12,
-                    R.drawable.bomb13, R.drawable.bomb14, R.drawable.bomb15,
-                    R.drawable.bomb16
-            };
+            items = new int[]{R.drawable.digital_num_02, R.drawable.digital_num_03, R.drawable.digital_num_04};
         }
 
         @Override
@@ -52,17 +44,15 @@ public class StackWidgetService extends RemoteViewsService {
         // 该方法的返回值控制各位置所显示的RemoteViews
         @Override
         public RemoteViews getViewAt(int position) {
-            // 创建RemoteViews对象，加载/res/layout目录下的widget_item.xml文件
-            RemoteViews rv = new RemoteViews(mContext.getPackageName(),
-                    R.layout.app_widget_list_item);
-            // 更新widget_item.xml布局文件中的widget_item组件
-            rv.setImageViewResource(R.id.widget_item,
-                    items[position]);
+            RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.app_widget_list_item);
+            rv.setImageViewResource(R.id.img, items[position]);
+            rv.setTextViewText(R.id.tv, String.valueOf(position + 1));
+
             // 创建Intent、用于传递数据
             Intent fillInIntent = new Intent();
-            fillInIntent.putExtra(StackWidgetProvider.EXTRA_ITEM, position);
+            fillInIntent.putExtra(AppWidgetProvider4List.EXTRA_ITEM, position);
             // 设置当单击该RemoteViews时传递fillInIntent包含的数据
-            rv.setOnClickFillInIntent(R.id.widget_item, fillInIntent);
+            rv.setOnClickFillInIntent(R.id.img, fillInIntent);
             // 此处让线程暂停0.5秒来模拟加载该组件
             try {
                 System.out.println("加载【" + position + "】位置的组件");
