c61d2e4db438aa95c00c18adf035e31e6768acaf
Vicky <ying.vicky.cao@outlook.com>
2019-08-09 15:17:30 +0800

    add lib version

--- soruce/TDDDemo/app/src/main/java/com/example/hades/tdd/junit/MainActivity.java
+++ soruce/TDDDemo/app/src/main/java/com/example/hades/tdd/junit/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.hades.tdd.junit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.hades.tdd.R;
 
