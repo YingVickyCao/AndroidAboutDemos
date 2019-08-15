30776557fbdb7966f4821c2b7a3ea556ce37053b
Vicky <ying.vicky.cao@outlook.com>
2019-08-15 12:37:13 +0800

    response

---
+++ soruce/android-about-demos/app/src/main/java/com/hades/example/android/other_ui/manager_phone_desktop/shortcuts/_static/StaticShortcutsResponseActivity.java
@@ -0,0 +1,16 @@
+package com.hades.example.android.other_ui.manager_phone_desktop.shortcuts._static;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.hades.example.android.R;
+
+public class StaticShortcutsResponseActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.static_shorts_layout);
+    }
+}
+
